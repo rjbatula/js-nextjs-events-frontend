2674a7406b1723fe7ef4695f24efdff24d6fc8ef
Ralph Joshua <rjbatula@yahoo.com>
2021-09-07 09:23:45 +0800

    Added getServerSideProps and getStaticProps on index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({ events }) {
 	return (
 		<div>
 			<Layout>
-				<h1>Home</h1>
+				<h1>Upcoming Events</h1>
 			</Layout>
 		</div>
 	)
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`)
+	const events = await res.json()
+
+	return {
+		props: { events },
+		revalidate: 1,
+	}
 }
