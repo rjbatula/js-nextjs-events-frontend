bf1110b391daa68bb825f4a04878cf5399b7120b
Ralph Joshua <rjbatula@yahoo.com>
2021-09-13 16:06:20 +0800

    Added Login functionality for with login api route

--- pages/account/login.js
+++ pages/account/login.js
@@ -13,6 +13,8 @@ export default function LoginPage() {
 
 	const { login, error } = useContext(AuthContext)
 
+	useEffect(() => error && toast.error(error))
+
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 		login({ email, password })
